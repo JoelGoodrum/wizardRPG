8c6c50c0d9b006dbd85f76fe04c3ad2db9d28156
Joel <joelgoodrum@hotmail.com>
2018-12-15 14:37:00 -0500

    bot can defend and attack player attacks

--- wizardlogic.js
+++ wizardlogic.js
@@ -8,17 +8,45 @@ let botVal = {
 	magic:100
 };
 
-function attackBot() {
-	botVal.health -= 10;
-	alert(`bot lost 10hp`);
-	updateGame();
-}
-
 function updateGame() {
 	document.getElementById('playerHealth').innerText = `health: ${playerVal.health}`;
 	document.getElementById('playerMagic').innerText = `magic: ${playerVal.magic}`;
 	document.getElementById('botHealth').innerText = `health: ${botVal.health}`;
-	document.getElementById('botMagic').innerText = `health: ${botVal.magic}`;
+	document.getElementById('botMagic').innerText = `magic: ${botVal.magic}`;
 }
+
+function attackBot() {
+
+	let valDecide = Math.floor(Math.random() * 2) //returns random number between 0 and 1
+	if(valDecide < 1) {
+		//bot attacks
+		botVal.health -= 10;
+		playerVal.health -= 10;
+		alert(`you attacked, bot lost 10hp
+bot attacked, you lost 10hp`);
+	} 
+	else {
+		//bot defend
+		playerVal.magic -= 3;
+		botVal.health += 3;
+		botVal.magic -= 20;
+		alert(`you attacked, you lost 3 magic
+bot defends, bot lost 20 magic, bot gain 3hp`)
+	}
+
+	
+	updateGame();
+}
+
+function botReact() {
+	let valDecide = Math.floor(Math.random() * 2); //returns random number between 0 and 1
+	if(valDecide === 0) {
+		//bot attack
+	} else {
+		//bot defend
+	}
+}
+
+
 
 updateGame();
