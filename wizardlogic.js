97da91b4bd6cd8c77f567bffe3f376e580dd2861
Joel <joelgoodrum@hotmail.com>
2018-12-15 14:02:48 -0500

    assign strings health and magic to script variables

--- wizardlogic.js
+++ wizardlogic.js
@@ -1,9 +1,17 @@
 let playerVal = {
-	health : 100,
-	magic : 100
+	health:100,
+	magic:100
+};
+let botVal = {
+	health:100,
+	magic:100
 };
 
-let botVal = {
-	health : 100,
-	magic : 100
-};
+function updateGame() {
+	document.getElementById('playerHealth').innerText = `health: ${playerVal.health}`;
+	document.getElementById('playerMagic').innerText = `magic: ${playerVal.magic}`;
+	document.getElementById('botHealth').innerText = `health: ${botVal.health}`;
+	document.getElementById('botMagic').innerText = `health: ${botVal.magic}`;
+}
+
+updateGame();
