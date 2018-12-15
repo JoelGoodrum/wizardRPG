fdd4dc3eb53c69e53410a7dc7f36c5471d6219a7
Joel <joelgoodrum@hotmail.com>
2018-12-15 14:15:53 -0500

    attack button subtracts health

--- wizardlogic.js
+++ wizardlogic.js
@@ -2,10 +2,17 @@ let playerVal = {
 	health:100,
 	magic:100
 };
+
 let botVal = {
 	health:100,
 	magic:100
 };
+
+function attackBot() {
+	botVal.health -= 10;
+	alert(`bot lost 10hp`);
+	updateGame();
+}
 
 function updateGame() {
 	document.getElementById('playerHealth').innerText = `health: ${playerVal.health}`;
